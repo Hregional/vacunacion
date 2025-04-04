e384a9f5c6c6a3a3d957746e1469fb724842ffac
adolfin <martin_aj22@hotmail.com>
2025-04-04 14:43:07 -0600

    feat: Add session management and Keycloak authentication to the backend

--- backend-ts/src/index.ts
+++ backend-ts/src/index.ts
@@ -1,12 +1,40 @@
 // src/index.ts
 import express, { Request, Response } from 'express';
 import { getServerCapabilities, createPatient } from './services/fhirService';
+import session from 'express-session';
+import Keycloak from 'keycloak-connect';
 
 const app = express();
+const memoryStore = new session.MemoryStore();
 const port = 3000;
+
+app.use(session({
+  secret: 'mi-secreto', // Cambia esto por algo seguro
+  resave: false,
+  saveUninitialized: true,
+  store: memoryStore,
+}));
+
+const keycloakConfig = {
+  'confidential-port': 0,
+  'auth-server-url': 'http://localhost:8080', // Base URL of Keycloak server
+  resource: 'vacunas-backend', // Correct property name for KeycloakConfig
+  'ssl-required': 'external',
+  realm: 'myrealm',
+  'bearer-only': true
+};
+
+
+const keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
+
+app.use(keycloak.middleware());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('¡Hola desde Express con TypeScript!');
+});
+
+app.get('/protegido', keycloak.protect(), (req, res) => {
+  res.send('Este recurso está protegido y solo accesible con un token válido.');
 });
 
 // Ruta para obtener capacidades
